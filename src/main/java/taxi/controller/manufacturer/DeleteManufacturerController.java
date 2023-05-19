df5d3ef59bc45c86338a9ea0dd33f21bedf11232
ASelivonchyk <aselivonchyk@gmail.com>
2023-05-19 15:46:29 +0300

    added project java classes.

---
+++ src/main/java/taxi/controller/manufacturer/DeleteManufacturerController.java
@@ -0,0 +1,23 @@
+package taxi.controller.manufacturer;
+
+import taxi.lib.Injector;
+import taxi.service.ManufacturerService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/manufacturers/delete")
+public class DeleteManufacturerController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final ManufacturerService manufacturerService = (ManufacturerService) injector
+            .getInstance(ManufacturerService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        manufacturerService.delete(Long.parseLong(req.getParameter("id")));
+        resp.sendRedirect(req.getContextPath() + "/manufacturers");
+    }
+}
