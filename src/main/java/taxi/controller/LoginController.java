6f37b5484efd480379e51f2ad5e8e5871b462631
ASelivonchyk <aselivonchyk@gmail.com>
2023-05-23 23:04:35 +0300

    - changed log level to Error; - codestyle fixes;

--- src/main/java/taxi/controller/LoginController.java
+++ src/main/java/taxi/controller/LoginController.java
@@ -1,6 +1,7 @@
 package taxi.controller;
 
 import static taxi.Constants.ERROR_MESSAGE_TAG;
+import static taxi.Constants.MAIN_PACKAGE_NAME;
 import static taxi.Constants.SUCCESS_MESSAGE_TAG;
 
 import java.io.IOException;
@@ -9,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Injector;
 import taxi.model.Driver;
@@ -17,7 +17,7 @@ import taxi.service.AuthenticationService;
 
 @WebServlet(urlPatterns = "/")
 public class LoginController extends HttpServlet {
-    private static final Injector injector = Injector.getInstance("taxi");
+    private static final Injector injector = Injector.getInstance(MAIN_PACKAGE_NAME);
     private final AuthenticationService authenticationService =
             (AuthenticationService) injector.getInstance(AuthenticationService.class);
 
@@ -26,8 +26,7 @@ public class LoginController extends HttpServlet {
             throws ServletException, IOException {
         if (req.getSession().getAttribute("driver_id") == null) {
             req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
-            req.getSession().setAttribute(ERROR_MESSAGE_TAG, null);
-            req.getSession().setAttribute(SUCCESS_MESSAGE_TAG, null);
+            deleteInfoMessagesFromSession(req);
         }
         resp.sendRedirect("/index");
     }
@@ -35,16 +34,19 @@ public class LoginController extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String login = req.getParameter("login");
-        String password = req.getParameter("password");
         try {
-            Driver driver = authenticationService.login(login, password);
-            HttpSession session = req.getSession();
-            session.setAttribute("driver_id", driver.getId());
+            Driver driver = authenticationService.login(
+                    req.getParameter("login"), req.getParameter("password"));
+            req.getSession().setAttribute("driver_id", driver.getId());
             resp.sendRedirect("/index");
         } catch (AuthenticationException e) {
             req.setAttribute("errorMsg", e.getMessage());
             req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
         }
+    }
+
+    private void deleteInfoMessagesFromSession(HttpServletRequest req) {
+        req.getSession().setAttribute(ERROR_MESSAGE_TAG, null);
+        req.getSession().setAttribute(SUCCESS_MESSAGE_TAG, null);
     }
 }
